e91c3a1159e1b5bbab6e1cd47e3e0fdaae7e0c46
Cameron Cianciolo <cameron.grace.cianciolo@gmail.com>
2023-02-18 11:34:38 -0600

    example comment

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -91,7 +91,19 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during test mode. */
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+
+
+
+
+    //this is where the code goes 
+
+
+
+
+
+    
+  }
 
   /** This function is called once when the robot is first started up. */
   @Override
