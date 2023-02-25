051dac4f89fcb3564e48b5f5b459cba0e7c1aba2
zkay77 <zckstfn@gmail.com>
2023-02-25 11:47:15 -0600

    Finished Solenoid & Compressor Commands

--- src/main/java/frc/robot/commands/CompressorControl.java
+++ src/main/java/frc/robot/commands/CompressorControl.java
@@ -5,36 +5,35 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import  frc.robot.subsystems.PneumaticsSubsystem;
 
-public class SolenoidControl extends CommandBase {
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.PneumaticsSubsystem;
+
+public class CompressorControl extends CommandBase {
   final PneumaticsSubsystem subsystem;
-  
-  /** Creates a new SolenoidControl. */
-  public SolenoidControl(PneumaticsSubsystem pneumaticsSubsystem) {
+  /** Creates a new CompressorControl. */
+  public CompressorControl(PneumaticsSubsystem pneumaticsSubsystem) {
     // Use addRequirements() here to declare subsystem dependencies.
-  
+
     subsystem = pneumaticsSubsystem;
     addRequirements(pneumaticsSubsystem);
-  
+
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {}
 
-final XboxController xbox = new XboxController(0);
+  final XboxController xbox = new XboxController(0);
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if (xbox.getXButton()){
-subsystem.openSolenoid();
-    }else {
-subsystem.closeSolenoid();
+    if (xbox.getXButton()) {
+      subsystem.turnOnCompressor();
+    }else{
+      subsystem.turnOffCompressor();
     }
-    
   }
 
   // Called once the command ends or is interrupted.
