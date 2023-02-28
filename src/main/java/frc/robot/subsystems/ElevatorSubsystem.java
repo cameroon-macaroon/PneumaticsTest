ba6e13c98769cf1ec97c886b9a2c9a44fbf10942
Cameron Cianciolo <cameron.grace.cianciolo@gmail.com>
2023-02-27 20:30:54 -0600

    fixed error

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -17,7 +17,7 @@ public class ElevatorSubsystem extends SubsystemBase {
   public void periodic() {
     // This method will be called once per scheduler run
   }
-  CANSparkMax elevatorMotor = new CANSparkMax(1,MotorType.kBrushless);
+  static CANSparkMax elevatorMotor = new CANSparkMax(1,MotorType.kBrushless);
   DigitalInput topSwitch = new DigitalInput(0);
   DigitalInput bottomSwitch = new DigitalInput(1);
 
